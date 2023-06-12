b4ab2d6e91e54caafe9630f960aed602976f8a78
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-06-11 23:19:08 -0500

    Repository and Environments
    The implementation of the repository of the journey entity and the environment variables is done

---
+++ src/data/journey/journey-implementation.repository.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { JourneyVmModel } from "src/domain/models/journey-vm.model";
+import { JourneyRepository } from "src/domain/repositories/journey.repository";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class JourneyImplementationRepository extends JourneyRepository {
+
+
+    constructor(private http: HttpClient ){
+        super();
+    }
+
+    GetJourneyAsync(params : {origin : string, destination : string, limit: number}): Observable<JourneyVmModel[]> {
+        return this.http.get<JourneyVmModel[]>(`${environment.url_backend_newshoreAir}/${params.origin}/${params.destination}/${params.limit}`);
+    }
+
+}
