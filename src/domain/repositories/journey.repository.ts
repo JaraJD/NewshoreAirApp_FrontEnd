87b948fde86284d3c9493f6ee174aa42e9e8d6ee
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-06-11 23:12:39 -0500

    Repository
    The implementation of the journey entity repository is carried out

---
+++ src/domain/repositories/journey.repository.ts
@@ -0,0 +1,6 @@
+import { Observable } from "rxjs";
+import { JourneyVmModel } from "../models/journey-vm.model";
+
+export abstract class JourneyRepository {
+    abstract GetJourneyAsync( params : {origin : string, destination : string, limit: number}) : Observable<JourneyVmModel[]>
+}
