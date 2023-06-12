8412ad72a0c12aa53738764f63c5a715c7f8a99c
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-06-12 00:26:47 -0500

    Index
    Implementation of index home page

--- src/presentation/app/modules/main/app.module.ts
+++ src/presentation/app/modules/main/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
